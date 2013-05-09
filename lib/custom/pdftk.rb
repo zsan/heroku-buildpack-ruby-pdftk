07a160fce1c23fd8ac53829bd4f90293fb9ef62c
scottmotte <scott@scottmotte.com>
2013-05-09 10:25:00 -0700

    Change url to location of my pdftk

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -6,7 +6,7 @@ class Pdftk < BaseCustom
     "pdftk"
   end
   def source_url
-    "http://s3.amazonaws.com/source_url_here"
+    "http://signatureproduction.s3.amazonaws.com/pdftk.tar.gz"
   end
   def used?
     File.exist?("#{build_path}/bin/pdftk") && File.exist?("#{build_path}/bin/lib/libgcj.so.12")
