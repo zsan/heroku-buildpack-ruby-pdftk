ce1c2e0be68686144174f78d9f65933c1012cda3
zsan <zaksantanu@gmail.com>
2014-07-11 23:53:38 +0700

    fixed

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -6,7 +6,7 @@ class Pdftk < BaseCustom
     "pdftk"
   end
   def source_url
-    "http://signatureproduction.s3.amazonaws.com/pdftk.tar.gz"
+    "https://s3.amazonaws.com/visas-api/pdftk.tar.gz"
   end
   def used?
     File.exist?("#{build_path}/bin/pdftk") && File.exist?("#{build_path}/bin/lib/libgcj.so.12")
